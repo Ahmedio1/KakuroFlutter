da860b7049939e18d9e03dc984e16fcc512efab5
Ahmedio__ <ahmed__daoud@outlook.fr>
2023-04-19 15:08:42 +0200

    "send Dart et pubsec maj"

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
