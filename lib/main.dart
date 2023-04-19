c62f2933ad66dfa418ffe00b0da531af8e0e828a
Ahmedio__ <ahmed__daoud@outlook.fr>
2023-04-19 14:43:12 +0200

    modif main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(const MyApp());
